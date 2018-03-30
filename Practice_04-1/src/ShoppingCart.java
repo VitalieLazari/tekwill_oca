85c7c3428be6cfd33e89e3e1f905b2bcbb743d85
vitalielazari <vitalie.lazari@gmail.com>
2018-03-30 18:54:01 +0200

    Gun updt

--- Practice_04-1/src/ShoppingCart.java
+++ Practice_04-1/src/ShoppingCart.java
@@ -3,9 +3,9 @@ public class ShoppingCart {
 	public static void main (String [] args) {
 	
 	String custName = "Tommy Gun";
-	String itemName = "Desert Eagle Calibre. 50";
+	String itemName = "Desert Eagle Cal. 50";
 	
 	System.out.println(custName + " wanna buy a new branded " + itemName);
-	}
+	} 
 
 }
