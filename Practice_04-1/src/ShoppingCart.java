dfe4f3fa3eae6211b47024076e3e586bb686a6e1
vitalielazari <vitalie.lazari@gmail.com>
2018-03-30 18:43:21 +0200

    Practice 4

---
+++ Practice_04-1/src/ShoppingCart.java
@@ -0,0 +1,11 @@
+public class ShoppingCart {
+	
+	public static void main (String [] args) {
+	
+	String custName = "Tommy Gun";
+	String itemName = "Desert Eagle Cab. 5";
+	
+	System.out.println(custName + " wanna buy a new branded " + itemName);
+	}
+
+}
