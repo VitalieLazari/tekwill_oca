51a7e280f9c7376eb5683733c9ddb1e11727582d
vitalielazari <vitalie.lazari@gmail.com>
2018-04-02 19:50:46 +0200

    lesson5

--- Practice_04-1/src/ShoppingCart.java
+++ Practice_04-1/src/ShoppingCart.java
@@ -6,6 +6,17 @@ public class ShoppingCart {
 	String itemName = "Desert Eagle Cal. 50";
 	
 	System.out.println(custName + " wanna buy a new branded " + itemName);
-	} 
 
+
+	String [] array = new String[10];
+	
+	array[0] = "a";
+	array[1] = "b";
+	array[2] = "c";
+	array[3] = "d";
+	
+	for (String string : array) {
+		System.out.println(string);
+	}
+}
 }
